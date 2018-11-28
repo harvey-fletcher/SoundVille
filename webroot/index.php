36844c87f94c78a38c6225a6994952bb143ffe57
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-11-28 18:59:40 +0000

    Initial Commit

---
+++ webroot/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="main.css" type="text/css"/>
+		<title>Linkenfest 2019</title>
+	</head>
+	<body>
+		<img src="http://files.linkenfest.co.uk/logo_png.png" class="main-logo"/>
+		<div class="links" align="right">
+			Home
+		</div>
+	</body>
+</html>
