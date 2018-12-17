24a65c85208fc680218850cf74d6e99f3299bf9a
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-12-17 14:48:36 +0000

    Remove gallery option from the menu as it does not exist

--- webroot/menu.php
+++ webroot/menu.php
@@ -9,5 +9,5 @@
 <a href="tickets.php" class="pageLink">Buy Tickets</a>
 <a href="bands.php" class="pageLink">Perform</a>
 <a href="volunteer.php" class="pageLink">Volunteer</a>
-<a href="gallery.php" class="pageLink">Photos</a>
+<!-- <a href="gallery.php" class="pageLink">Photos</a> -->
 <a href="contact.php" class="pageLink">Contact</a>
